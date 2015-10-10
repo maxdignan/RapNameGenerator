b4fa4923ea4b0f1c014bfe43e80e1b6cef555088
maxdignan <maxdignan@gmail.com>
2015-10-10 13:59:20 -0400

    added in rap name for first or last name

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 
 
-/** 
+/**
  *  RAP NAME GENERATOR
  *  The user will insert their first name and on click receive one of several
  *  possible outputs (i.e. Jill).
@@ -18,6 +18,21 @@ function Generator() {
     this.last_names = ['the Chef', 'Digital', 'Wise', 'Knight', 'Wrecka', 'the Genius', 'the Zoo Keeper', 'the Monk', 'the Scientist', 'the Disciple', 'the Darkman', 'Pellegrino', 'the Ill Figure', 'Rocks The World', 'the Baptist',];
     this.first_names = ['Inspectah', 'Masta', 'Poppa', 'Five Foot', 'Ghostface', 'Old Dirty'];
 
+    this.getName = function(arr) {
+        return arr[Math.floor(Math.random() * arr.length)];
+    };
+
+    this.getWrapName = function(name) {
+        var randValue = Math.round(Math.random());
+        if (randValue == 0) { //name is first name
+            var lastName = this.getName(this.last_names);
+            return name + " " + lastName;
+        } else { //name entered is last name
+            var firstName = this.getName(this.first_names);
+            return firstName + " " + name;
+        }
+    };
+
 }
 
 
@@ -26,7 +41,25 @@ function Generator() {
 
 $(document).ready(function() {
 
-    var engine = new Generator;
-    //Add your codez here
+    var engine = new Generator();
+
+    $('#enter').on('click', function(e) {
+        var name = $('#user-input').val();
+
+
+        $("#err").hide();
+        $("#res").hide();
+
+        if (name.length == 0) {
+            $("#err").show();
+            //$("#err").html(engine.getValue(name));
+        } else {
+            $("#res").show();
+            $("#res").html(engine.getWrapName(name));
+        }
+
+
+    });
+
 
 });
